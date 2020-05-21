9d493b4a6bc655de627feb8a2939afae806513b5
chaninkou <michaelkoulove@yahoo.com>
2020-05-20 21:24:55 -0700

    Redid problem on getting product with 2 steps.

--- src/productOfArrays/FindProductOfArrayExceptSelfFunction.java
+++ src/productOfArrays/FindProductOfArrayExceptSelfFunction.java
@@ -10,7 +10,7 @@ public class FindProductOfArrayExceptSelfFunction {
         result[0] = 1;
         
         for(int i = 1; i < nums.length; i++){
-        	// This will get whatever product of the left side only
+        	// Get product of everything on left side only, excluding current element
             leftProduct *= nums[i - 1];
             
             result[i] = leftProduct;
@@ -18,12 +18,11 @@ public class FindProductOfArrayExceptSelfFunction {
         
         int rightProduct = 1;
         
-        // Does not start from last index since there is nothing on the right side
         for(int j = nums.length - 2; j >= 0; j--){
         	// Will get whatever product from right
             rightProduct *= nums[j + 1];
             
-            // With the product of right, multiply the left product now
+            // With the product of right, multiply the left product from result
             result[j] *= rightProduct;
         }
         
